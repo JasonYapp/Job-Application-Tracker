9e7da66a7295603ef524440b165ea7850b475e31
JasonYapp <jyaprox@gmail.com>
2025-06-09 17:33:57 +1000

    Basic nav bar implemented

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -9,13 +9,22 @@ const Navbar = () => {
     return (
         
         <nav className="nav">
-            <Link to="/" className="site-title">Site Name</Link>
-            <ul>
+            <Link to="/" className="MissionEmployment">Mission Employment</Link>
+            <ul className="nav-center">
                 <li>
-                    <Link to="/about">About</Link>
+                    <Link className="navLink" to="/about">About</Link>
                 </li>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link className="navLink" to="/">Home</Link>
+                </li> 
+            </ul>
+
+             <ul className="nav-right">
+                <li>
+                    <Link className="navLink" to="/">Log In</Link>
+                </li>
+                <li>
+                    <Link className="navLink" to="/">Sign Up For Free</Link>
                 </li>
             </ul>
         </nav>
