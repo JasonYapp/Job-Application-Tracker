8fa982a9385b1737f66d46fc4d9bd721125d8b23
JasonYapp <jyaprox@gmail.com>
2025-06-12 22:23:40 +1000

    Basic login page skeleton with minor styling

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,10 +1,59 @@
+import '../css/login.css';
+
+
+
+const handleSubmit = async () => {
+
+}
+
 const Login = () => {
     return(
         
         <>
-            <h1>Login page</h1>
-            
-            <p>Email, password, google/apple sign in</p>
+            <section>
+                <div style={{ paddingTop: '8rem' }}>
+                    <div className='container'>
+                        <header>Login</header>
+
+
+                        <p className="tagline">Please enter your details to login to your account</p>
+
+                        {" "}
+
+                        <div className="form">
+                            <input
+                                type="email"
+                                name="email"
+                                // value={formData.email}
+                                // onChange={handleInputChange}
+                                placeholder="Enter your email"
+                                onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)}
+                            />
+
+                            <input
+                                type="password"
+                                name="password"
+                                // value={formData.password}
+                                // onChange={handleInputChange}
+                                placeholder="Enter your password"
+                                onKeyDown={(e) => e.key === 'Enter' && handleSubmit(e)}
+                            />
+                        </div>
+
+                        <div className='submitButton'>
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                // disabled={isLoading}
+                            >
+                                Log in
+                            </button>
+                        </div>
+  
+                    </div>
+                </div>
+
+            </section>
         </>
        
     );
