421172bc2f02068af2cdd717eab9999b3fc204a1
JasonYapp <jyaprox@gmail.com>
2025-06-16 22:15:28 +1000

    css changes

--- Frontend/app/routes/signup.tsx
+++ Frontend/app/routes/signup.tsx
@@ -1,12 +1,65 @@
+import { useState, type FormEvent } from 'react';
+import { useNavigate } from 'react-router';
+
+import '../css/signup.css';
+
+
+
 const Signup = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log("Email: " + email + "Password: " + password);
+
+        // navigate('/routehere');
+    }
+
     return(
         
         <>
-            <h1>Signup page</h1>
-            
-            <p>Need text fields, password, pass security, text authentication?</p>
+            <section>
+                <div style={{ paddingTop: '8rem' }}>
+                    <div className='signUpContainer'>
+                        <h1 className="header">Sign Up</h1>
 
-            <p>Sign in with google/apple</p>
+                        <p className="tagline">Please enter your details to sign up!</p>
+
+                        
+                        <form onSubmit={handleSubmit}>
+                            <div className="form">
+                                <input
+                                    type="email"
+                                    name="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    placeholder="Enter your email"
+                                    required
+                                />
+
+                                <input
+                                    type="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    placeholder="Enter your password"
+                                />
+                            </div>
+                        
+                            <div className='submitButton'>
+                                <button type="submit"> Sign Up </button>
+                            </div>
+                        </form>
+
+                        <h2> or </h2>
+  
+                    </div>
+                </div>
+
+            </section>
         </>
        
     );
