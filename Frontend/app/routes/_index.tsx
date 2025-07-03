fd9064121d580e399e4ee21bbad8dfccbf2b86c7
JasonYapp <jyaprox@gmail.com>
2025-07-04 01:42:09 +1000

    Revamped landing page

--- Frontend/app/routes/_index.tsx
+++ Frontend/app/routes/_index.tsx
@@ -1,34 +1,67 @@
 import '../css/index.css';
+import KanBanView from '../images/KanBanView.png';
+import UpcomingTasks from '../images/UpcomingTasks.png';
 
 
 const Home = () => {
+
+
+    const signUp = async () => {
+        
+    }
+
     return(
         
         <>
+        <div className='landing-container'>
+        <div className="bg-element"></div>
+        <div className="bg-element"></div>
+        <div className="bg-element"></div>
+        <div className="pulse-element"></div>
+        <div className="pulse-element"></div>
+        <div className="pulse-element"></div>
 
-        <div style={{ paddingTop: '6rem' }}>
-            <div className="container">
+            
+                <div className="box-container">
 
-                <section>
-                <header> From application to offer - every step in one place.</header>
+                    <section className="first-box">
+                        <header> From application to offer - every step in one place.</header>
 
-                    <div className="tagline">
-                        <p>With details and reminders on interviews, deadlines and those pesky questionaires, Mission Employment is job tracking made simple.</p>
-                        <p> Sign up button here?</p>
-                    </div>
-                </section>
+                            <div className="tagline">
+                                <p>With details and reminders on interviews, deadlines and those pesky questionaires, Mission Employment is job tracking made simple.</p>
+                            </div>
 
-                <section className='separate'>
-                    <p> Photo view of Kanban timeline here?</p>
-                </section>
+                        <button className="sign-up">Start Your Journey</button>
+                    </section>
 
-                <section className='separate'>
-                    <p> One more image off to the side, with text alongside it to explain one more benefit. </p>
-                    <p> "A tool to keep you organised. The days of being overwhelmed are gone. With text reminders and calendar integration, never miss another opportunity.</p>
-                </section>
+                    <section className='second-box'>
+                        <div className='second-box-content'>
+                            <h3>Stay Organized, <br></br>Stay Ahead </h3>
+                            <p> A tool to keep you organised. The days of being overwhelmed are gone. With text reminders and calendar integration, never miss another opportunity.</p>
+                        </div>
 
+                        <div className="second-box-task">
+                            <img src={UpcomingTasks} alt="Task Image" />
+                        </div>
+                    </section>
+
+                    <section className='third-box'>
+                        <div className='third-box-content'>
+                            <h3>Visual Progress Tracking</h3>
+                            <p>See your entire job search journey at a glance with our intuitive Kanban-style board. 
+                                Track applications from submission to offer, 
+                                with clear visual indicators of your progress at each stage.
+                            </p>
+                        </div>
+
+                        <div className="third-box-kanban">
+                            <img src={KanBanView} alt="Kanban Board" />
+                        </div>
+                    </section>
+
+                </div>
             </div>
-        </div>
+        
         
         
 
